c68d66d79e7fb262c798d6f4d93d9b1bb3fdfa0b
N00b-Sk11L <m.kohen2008@gmail.com>
2026-01-03 19:20:43 +0200

    Update JoystickSender.ino

--- JoystickSender.ino
+++ JoystickSender.ino
@@ -4,19 +4,15 @@
 
 const int PIN_J1_X = 34;
 const int PIN_J1_Y = 35;
-const int PIN_J1_BTN = 32;
 
 const int PIN_J2_X = 33;
 const int PIN_J2_Y = 25;
-const int PIN_J2_BTN = 26;
 
 typedef struct {
   int j1x;
   int j1y;
-  int j1btn;
   int j2x;
   int j2y;
-  int j2btn;
 } joystickData;
 
 joystickData myData;
@@ -31,8 +27,6 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 void setup() {
   Serial.begin(115200);
   
-  pinMode(PIN_J1_BTN, INPUT_PULLUP);
-  pinMode(PIN_J2_BTN, INPUT_PULLUP);
 
   WiFi.mode(WIFI_STA);
 
@@ -52,11 +46,9 @@ void setup() {
 void loop() {
   myData.j1x = analogRead(PIN_J1_X);
   myData.j1y = analogRead(PIN_J1_Y);
-  myData.j1btn = digitalRead(PIN_J1_BTN);
 
   myData.j2x = analogRead(PIN_J2_X);
   myData.j2y = analogRead(PIN_J2_Y);
-  myData.j2btn = digitalRead(PIN_J2_BTN);
 
   esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
   
