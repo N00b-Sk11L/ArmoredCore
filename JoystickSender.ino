07271bde32f2050a4a4ebfc22927479d672a381b
N00b-Sk11L <m.kohen2008@gmail.com>
2026-01-03 19:01:25 +0200

    Update JoystickSender.ino

--- JoystickSender.ino
+++ JoystickSender.ino
@@ -1,43 +1,64 @@
+#include <Arduino.h>
 #include <WiFi.h>
-#include <WiFiUdp.h>
+#include <esp_now.h>
 
-const char* ssid = "ARMORED_NET";
-const char* password = "12345678";
+const int PIN_J1_X = 34;
+const int PIN_J1_Y = 35;
+const int PIN_J1_BTN = 32;
 
-WiFiUDP udp;
-const char* receiverIP = "192.168.4.2";
-const int udpPort = 4210;
+const int PIN_J2_X = 33;
+const int PIN_J2_Y = 25;
+const int PIN_J2_BTN = 26;
 
-const int joy1X = 0;
-const int joy1Y = 1;
-const int joy2X = 2;
-const int joy2Y = 3;
+typedef struct {
+  int j1x;
+  int j1y;
+  int j1btn;
+  int j2x;
+  int j2y;
+  int j2btn;
+} joystickData;
+
+joystickData myData;
+
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+}
 
 void setup() {
   Serial.begin(115200);
-  pinMode(joy1X, INPUT);
-  pinMode(joy1Y, INPUT);
-  pinMode(joy2X, INPUT);
-  pinMode(joy2Y, INPUT);
+  
+  pinMode(PIN_J1_BTN, INPUT_PULLUP);
+  pinMode(PIN_J2_BTN, INPUT_PULLUP);
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  if (esp_now_init() != ESP_OK) 
+    return;
+ 
+  esp_now_register_send_cb(OnDataSent);
+
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+    return;
 }
 
 void loop() {
-  int j1x = analogRead(joy1X);
-  int j1y = analogRead(joy1Y);
-  int j2x = analogRead(joy2X);
-  int j2y = analogRead(joy2Y);
+  myData.j1x = analogRead(PIN_J1_X);
+  myData.j1y = analogRead(PIN_J1_Y);
+  myData.j1btn = digitalRead(PIN_J1_BTN);
 
-  char buffer[64];
-  snprintf(buffer, sizeof(buffer), "%d,%d,%d,%d", j1x, j1y, j2x, j2y);
+  myData.j2x = analogRead(PIN_J2_X);
+  myData.j2y = analogRead(PIN_J2_Y);
+  myData.j2btn = digitalRead(PIN_J2_BTN);
 
-  udp.beginPacket(receiverIP, udpPort);
-  udp.print(buffer);
-  udp.endPacket();
-
-  Serial.println(buffer);
-  delay(50);
+  esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+  
+  delay(20);
 }
